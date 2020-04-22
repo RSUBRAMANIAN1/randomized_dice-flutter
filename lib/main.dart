16933717fb1992d207022f3c7cd139fa63871179
RSUBRAMANIAN1 <r.saiprasad99@gmail.com>
2020-04-22 23:16:20 +0530

    dice randomized

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,71 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text(
+            'Dicee',
+          ),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: dice(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+
+
+class dice extends StatefulWidget {
+  dice({Key key}) : super(key: key);
+
+  @override
+  _diceState createState() => _diceState();
+}
+
+class _diceState extends State<dice> {
+    int dicno = 1;
+  int dicno1 = 2;
+  func(){
+     setState(() {
+                    dicno = Random().nextInt(6) + 1;
+                   dicno1= Random().nextInt(6) + 1;
+                 });
+
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+       child: Container(
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: func,
+                child: Image.asset(
+                  'images/dice$dicno1.png',
+                ),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: func,
+                child: Image.asset(
+                  'images/dice$dicno.png',
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+    );
   }
 }
